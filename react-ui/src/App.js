768dcb11b8fd5c4cb9ab31b238fee0b69041c07f
Vimal Jobanputra <vim@bitbybit.io>
2017-02-02 22:41:49 +1300

    Test commit to trigger build

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       message: null,
       fetching: true
     };
@@ -38,7 +38,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React and Node!</h2>
         </div>
         <p className="App-intro">
           {'This is '}
